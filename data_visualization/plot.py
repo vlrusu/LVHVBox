0eb1c537d6682bcf4cbfc6ebe27d37fc0bd060fa
Isaiah Wardlaw <91215285+radicalvictorian@users.noreply.github.com>
2022-07-07 10:58:58 -0500

    changed range xdata to time

--- data_visualization/plot.py
+++ data_visualization/plot.py
@@ -42,65 +42,65 @@ for i in split_lines:
 
 
 def plot_blade_voltage(channel):
-    plt.scatter(range(0,len(blade_voltage[channel])),blade_voltage[channel],s=3)
+    plt.scatter(time,blade_voltage[channel],s=3)
     plt.title("Blade Voltage as a Function of Time")
     plt.ylabel("Voltage (Volts)")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_blade_current(channel):
-    plt.scatter(range(0,len(blade_current[channel])),blade_current[channel],s=3)
+    plt.scatter(time,blade_current[channel],s=3)
     plt.title("Blade Current as a Function of Time")
     plt.ylabel("Current (" + chr(181) + ")")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_blade_temperature(channel):
-    plt.scatter(range(0,len(blade_temperature[channel])),blade_temperature[channel],s=3)
+    plt.scatter(time,blade_temperature[channel],s=3)
     plt.title("Blade Temperature as a Function of Time")
     plt.ylabel("Temperature (Celsius)")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_five_voltage(channel):
-    plt.scatter(range(0,len(five_voltage[channel])),five_voltage[channel],s=3)
+    plt.scatter(time,five_voltage[channel],s=3)
     plt.title("Board Five Volt Voltage as a Function of Time")
     plt.ylabel("Voltage (Volts)")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_five_current(channel):
-    plt.scatter(range(0,len(five_current[channel])),five_current[channel],s=3)
+    plt.scatter(time,five_current[channel],s=3)
     plt.title("Board Five Volt Current as a Function of Time")
     plt.ylabel("Current (" + chr(181) + ")")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_cond_voltage(channel):
-    plt.scatter(range(0,len(cond_voltage[channel])),cond_voltage[channel],s=3)
+    plt.scatter(time,cond_voltage[channel],s=3)
     plt.title("Conditioned Board Voltage as a Function of Time")
     plt.ylabel("Voltage (Volts)")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_cond_current(channel):
-    plt.scatter(range(0,len(cond_currnet[channel])),cond_current[channel],s=3)
+    plt.scatter(time,cond_current[channel],s=3)
     plt.title("Conditioned Board Current as a Function of Time")
     plt.ylabel("Current (" + chr(181) + ")")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_hv_voltage(channel):
-    plt.scatter(range(0,len(hv_voltage[channel])),hv_voltage[channel],s=3)
+    plt.scatter(time,hv_voltage[channel],s=3)
     plt.title("HV Voltage as a Function of Time")
     plt.ylabel("Voltage (Volts)")
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
 def plot_hv_current(channel):
-    plt.scatter(range(0,len(hv_current[channel])),hv_current[channel],s=3)
+    plt.scatter(time,hv_current[channel],s=3)
     plt.title("HV Current as a Function of Time")
     plt.ylabel("Current (" + chr(181) + ")")
     plt.show()
 
-plot_hv_voltage(0)
+plot_blade_voltage(0)
