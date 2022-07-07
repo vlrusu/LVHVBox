31a045270f70db559810b0adcce44e4fa9c44086
Isaiah Wardlaw <91215285+radicalvictorian@users.noreply.github.com>
2022-07-07 10:55:50 -0500

    added rudimentary hv plots

--- data_visualization/plot.py
+++ data_visualization/plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-file1 = open('logfile.txt','r')
+file1 = open('logfile4.txt','r')
 lines = file1.readlines()
 file1.close()
 
@@ -18,8 +18,12 @@ five_current=[[] for i in range(6)]
 cond_voltage=[[] for i in range(6)]
 cond_current=[[] for i in range(6)]
 
-initial_time=int(split_lines[0][48][:10])
+hv_voltage=[[] for i in range(12)]
+hv_current=[[] for i in range(12)]
+
+initial_time=int(split_lines[0][84][:10])
 time=[]
+
 
 for i in split_lines:
     for y in range(0,40,8):
@@ -30,7 +34,12 @@ for i in split_lines:
         five_current[int(y/8)].append(i[y+5])
         cond_voltage[int(y/8)].append(i[y+6])
         cond_current[int(y/8)].append(i[y+7])
-    time.append(int(i[48][:10])-initial_time)
+    for x in range(48,84,3):
+        hv_voltage[int((x-48)/3)].append(i[x+1])
+        hv_current[int((x-48)/3)].append(i[x+2])
+
+    time.append(int(i[84][:10])-initial_time)
+
 
 def plot_blade_voltage(channel):
     plt.scatter(range(0,len(blade_voltage[channel])),blade_voltage[channel],s=3)
@@ -81,4 +90,17 @@ def plot_cond_current(channel):
     plt.xlabel("Elapsed Time (Seconds)")
     plt.show()
 
-plot_blade_voltage(0)
+def plot_hv_voltage(channel):
+    plt.scatter(range(0,len(hv_voltage[channel])),hv_voltage[channel],s=3)
+    plt.title("HV Voltage as a Function of Time")
+    plt.ylabel("Voltage (Volts)")
+    plt.xlabel("Elapsed Time (Seconds)")
+    plt.show()
+
+def plot_hv_current(channel):
+    plt.scatter(range(0,len(hv_current[channel])),hv_current[channel],s=3)
+    plt.title("HV Current as a Function of Time")
+    plt.ylabel("Current (" + chr(181) + ")")
+    plt.show()
+
+plot_hv_voltage(0)
