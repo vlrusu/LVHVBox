56444bc5cd00f0fe0aa0d00b0d856a2004efd941
Isaiah Wardlaw <91215285+radicalvictorian@users.noreply.github.com>
2022-11-15 15:40:09 -0600

    Merge noGUI with pyqt4 gui2.py

--- gui2.py
+++ gui2.py
@@ -24,6 +24,8 @@ import logging
 import serial
 import matplotlib
 import sys
+from commands import *
+import cmd2
 
 
 from PyQt5.QtWidgets import (
@@ -54,12 +56,170 @@ background_color='background-color: white;'
 button_color='background-color: white;'
 
 
+def process_command(command):
+
+    app.async_alert ("Processing command ")
+    app.async_alert(' '.join(str(e) for e in command))
+    func = getattr(lvhvbox,command[0])
+    ret = func(command[1:])
+
+#this would be better as a real module, avoid global
+#    ret = globals()[command[0]](command[1:])
+
+    app.async_alert(' '.join(str(e) for e in ret))
+    return 0
+
+
+def lvloop():
+
+    while (1):
+        try:
+            lvdata = lvqueue.get(block=False)
+            app.async_alert("I got a LV command")
+            retc = process_command(lvdata)
+
+        except queue.Empty:
+            lvhvbox.loglvdata()
+            time.sleep(0.1)
+
+def hvloop1():
+
+    while (1):
+        try:
+            hvdata = hvqueue1.get(block=False)
+            app.async_alert("I got a HV command")
+            retc = process_command(hvdata)
+
+        except queue.Empty:
+            lvhvbox.loghvdata1()
+            time.sleep(0.1)
+
+
+def hvloop2():
+
+    while (1):
+        try:
+            hvdata = hvqueue2.get(block=False)
+            app.async_alert("I got a HV command")
+            retc = process_command(hvdata)
+
+        except queue.Empty:
+            lvhvbox.loghvdata2()
+            time.sleep(0.1)
+
+
+class CmdLoop(cmd2.Cmd):
+    """Example cmd2 application where we create commands that just print the arguments they are called with."""
+
+    def __init__(self):
+        # Create command shortcuts which are typically 1 character abbreviations which can be used in place of a command. Leave as is
+        shortcuts = dict(cmd2.DEFAULT_SHORTCUTS)
+        shortcuts.update({'$': 'readvoltage', '%': 'readcurrent'})
+        super().__init__(shortcuts=shortcuts)
+
+
+    # There has to be a command for every interaction we need. So, readvoltage, readcurrents, readtemps, etc.
+    # Each one has to have its counterpart in commands.py
+
+    # readvoltage()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_readvoltage(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel])
+
+    # readcurrent()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_readcurrent(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel])
+
+    # readtemp()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_readtemp(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel])
+
+    # test()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-v', '--voltage', type=float, help='Volatge to ramp to')
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+
+    @cmd2.with_argparser(pprint_parser)
+    def do_test(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel, args.voltage])
+
+
+    # powerOn()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_powerOn(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel])
+
+    # powerOff()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_powerOff(self, args):
+        """Print the options and argument list this options command was called with."""
+        lvqueue.put([args.cmd2_statement.get().command, args.channel])
+
+    # ramp()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    pprint_parser.add_argument('-v', '--voltage', type=int, help='Volatge to ramp to')
+
+    @cmd2.with_argparser(pprint_parser)
+    def do_ramp(self, args):
+        """Print the options and argument list this options command was called with."""
+        hvqueue1.put([args.cmd2_statement.get().command, args.channel, args.voltage])
+    # down()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+
+    @cmd2.with_argparser(pprint_parser)
+    def do_down(self, args):
+        """Print the options and argument list this options command was called with."""
+        hvqueue1.put([args.cmd2_statement.get().command, args.channel])
+
+    # resetHV()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    @cmd2.with_argparser(pprint_parser)
+    def do_resetHV(self, args):
+        """Print the options and argument list this options command was called with."""
+        hvqueue1.put([args.cmd2_statement.get().command, args.channel])
+
+    # setHVtrip()
+    pprint_parser = cmd2.Cmd2ArgumentParser()
+    pprint_parser.add_argument('-c', '--channel', type=int, help='Channel number')
+    pprint_parser.add_argument('-T', '--trippoint', type=int, help='Trip point in nA')
+    @cmd2.with_argparser(pprint_parser)
+    def do_setHVtrip(self, args):
+        """Print the options and argument list this options command was called with."""
+        hvqueue1.put([args.cmd2_statement.get().command, args.channel, args.trippoint])
+
 
 class Window(QMainWindow):
-    def __init__(self,test):
+    def __init__(self,test,v48,i48,T48,hv_v,hv_i,hvpcbtemp):
         super(Window,self).__init__()
 
         self.test=test
+        self.v48=v48
+        self.i48=i48
+        self.T48=T48
+        self.hv_v=hv_v
+        self.hv_i=hv_i
+        self.hvpcbtemp=hvpcbtemp
+
 
 
         #window.setCursor(PyQt5.BlankCursor)
@@ -739,6 +899,30 @@ class Window(QMainWindow):
         self.hv_plot_canvas.draw()
         self.hv_plot_canvas.flush_events()
 
+    # updates the blade table with recent data
+    def update_blade_table(self):
+        for j in range(6):
+            self.blade_voltage_entries[j].setText(str(self.v48[j]))
+            self.blade_current_entries[j].setText(str(self.i48[j]))
+            self.blade_temperature_entries[j].setText(str(self.T48[j]))
+
+    # updates the board table with recent data (readmon data)
+    def update_board_table(self):
+        for j in range(6):
+            self.board_5v_voltage_entries[j].setText(str(self.five_voltage[j]))
+            self.board_5v_current_entries[j].setText(str(self.five_current[j]))
+            self.board_cond_voltage_entries[j].setText(str(self.cond_voltage[j]))
+            self.board_cond_current_entries[j].setText(str(self.cond_current[j]))
+
+    # updates the hv table with latest available data
+    def update_hv_table(self):
+        for j in range(12):
+            self.hv_voltage_entries[j].setText(str(self.hv_v[j]))
+            self.hv_current_entries[j].setText(str(self.hv_i[j]))
+
+    def update_data(self):
+        self.update_hv_table()
+        self.update_blade_table()
 
     def initialize_data(self):
         # keeps lv screen update from occuring until data is acquired
@@ -790,13 +974,19 @@ class Window(QMainWindow):
         self.hv_board_temp=0
         self.hv_board_current=0
 
+        self.table_update_timer = QTimer(self)
+        self.table_update_timer.setSingleShot(False)
+        self.table_update_timer.timeout.connect(self.update_data)
+        self.table_update_timer.start(1000)
+
+
 
 
 ## Main function
 ## =============
 
 if __name__ == '__main__':
-    '''
+
     history_file = os.path.expanduser('.lvhv_history')
     if not os.path.exists(history_file):
         with open(history_file, "w") as fobj:
@@ -808,16 +998,21 @@ if __name__ == '__main__':
 
 
     lvqueue = queue.Queue()
-    hvqueue = queue.Queue()
+    hvqueue1 = queue.Queue()
+    hvqueue2 = queue.Queue() # this is such a hack!!!!
 
     logging.basicConfig(filename='lvhvbox.log', format='%(asctime)s:%(levelname)s:%(message)s', encoding='utf-8', level=logging.DEBUG)
 
-    sertmp1 = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1,write_timeout = 1)
-    sertmp2 = serial.Serial('/dev/ttyACM1', 115200, timeout=0.1,write_timeout = 1)
+    sertmp1 = serial.Serial('/dev/ttyACM0', 115200, timeout=10,write_timeout = 1)
+    sertmp2 = serial.Serial('/dev/ttyACM1', 115200, timeout=10,write_timeout = 1)
 
     #decide which on the 1 and which is 2
     sertmp1.timeout = None
-    line = sertmp1.readline().decode('ascii')
+
+    line = ""
+    while(len(line)) <30:
+        line = sertmp1.readline().decode('ascii')
+
     whichone = line.split("|")[3][1]
     print("ser1 is " + str(whichone))
 
@@ -829,29 +1024,56 @@ if __name__ == '__main__':
         ser2 = sertmp1
 
     sertmp2.timeout = None
-    line = sertmp2.readline().decode('ascii')
+    line = ""
+    while(len(line)) < 30:
+        line = sertmp2.readline().decode('ascii')
+    #    print(len(line))
     whichone = line.split("|")[3][1]
     print("ser2 is " + str(whichone))
 
 
     lvlogname = "lvdata.log"
     lvlog = open(os.path.join(topdir,lvlogname),"w")
-    hvlogname = "hvdata.log"
-    hvlog = open(os.path.join(topdir,hvlogname),"w")
-    '''
+    hvlogname0 = "hvdata0.log"
+    hvlog0 = open(os.path.join(topdir,hvlogname0),"w")
+    hvlogname1 = "hvdata1.log"
+    hvlog1 = open(os.path.join(topdir,hvlogname1),"w")
 
-    '''
-    lvhvbox = LVHVBox(app,ser1,ser2,hvlog,lvlog)
+
+    app = CmdLoop()
+    lvhvbox = LVHVBox(app,ser1,ser2,hvlog0,hvlog1,lvlog)
+
 
     lvThrd = threading.Thread(target=lvloop, daemon = True)
     lvThrd.start()
+    hvThrd1 = threading.Thread(target=hvloop1, daemon = True)
+    hvThrd1.start()
+    hvThrd2 = threading.Thread(target=hvloop2, daemon = True)
+    hvThrd2.start()
 
-    #app.cmdloop()
+    time.sleep(4)
+
+    App = QApplication(sys.argv)
+
+
+
+    hv_i=lvhvbox.ihv0+lvhvbox.ihv1
+    hv_v=lvhvbox.vhv0+lvhvbox.vhv1
+
+    window = Window(True,lvhvbox.v48,lvhvbox.i48,lvhvbox.T48,hv_i,hv_v,lvhvbox.hvpcbtemp)
+
+    gui_thread = threading.Thread(target=App.exec(), daemon = True)
+    gui_thread.start()
+
+    app.cmdloop()
     lvlog.close()
     hvlog.close()
     sys.exit()
-    '''
+    sys.exit()
 
+
+    '''
     App = QApplication(sys.argv)
     window = Window(True)
     sys.exit(App.exec())
+    '''
