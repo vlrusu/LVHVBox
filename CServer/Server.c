c0418815e980c227afb278efadb53e455ca1b633
Isaiah Wardlaw <91215285+radicalvictorian@users.noreply.github.com>
2024-03-19 08:40:42 -0500

    Revert "proper close"
    This reverts commit e846243cfb0825f5b520d7eef0e65c62a02f1386.

--- CServer/Server.c
+++ CServer/Server.c
@@ -1894,10 +1894,8 @@ void sigintHandler(int sig_num) {
     libusb_unref_device(device_0);
     free(device_handle_0);
 
-    if (write_data == 1) {
-      fclose(fp_I_0);
-      fclose(fp_V_0);
-    }
+    fclose(fp_I_0);
+    fclose(fp_V_0);
   }
 
   if (use_pico1 == 1) {
@@ -1905,10 +1903,8 @@ void sigintHandler(int sig_num) {
     libusb_unref_device(device_1);
     free(device_handle_1);
 
-    if (write_data == 1) {
-      fclose(fp_I_1);
-      fclose(fp_V_1);
-    }
+    fclose(fp_I_1);
+    fclose(fp_V_1);
   }
 
   free(hvMCP);
