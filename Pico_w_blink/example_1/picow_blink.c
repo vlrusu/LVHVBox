4a3feee5da3aaa59bc8aac30dc62cf9301035a81
Ben Messerly <bamesserly@gmail.com>
2024-05-16 14:35:04 -0500

    Upgraded to a blink AND hello world test script.
    The hello world aspect of this doesn't work with tinyusb,
    which we use in production code.

--- Pico_w_blink/example_1/picow_blink.c
+++ Pico_w_blink/example_1/picow_blink.c
@@ -1,22 +1,21 @@
-/**
- * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include "pico/stdlib.h"
+// Blink pico led AND repeatedly print hello world.
+// 
+// Blink the led on a pico "w" (aka wifi-enabled) is different than on a
+// non-wifi pico. Pin access is different.
 #include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
 
 int main() {
-    stdio_init_all();
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed");
-        return -1;
-    }
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(250);
-    }
+  stdio_init_all();
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed"); // Don't think wifi is actually used tho.
+    return -1;
+  }
+  while (true) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
+    printf("Hello World\n");
+  }
 }
