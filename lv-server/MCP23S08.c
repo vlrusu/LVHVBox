30b5ccf183599673f04e28baa62cc960083cccff
Vadim Rusu <vadim.l.rusu@gmail.com>
2025-04-17 10:14:05 -0500

    add missing files

---
+++ lv-server/MCP23S08.c
@@ -0,0 +1,161 @@
+/**
+ * @file MCP23S08.c
+ * @author Vadim Rusu (vadim.l.rusu@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-10-07
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "MCP23S08.h" // Header files for this class
+
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include "gpio.h"
+#include "utils.h"
+
+
+
+#define MCPDELAY 1
+
+// Control byte and configuration register information - Control Byte: "0100 A2 A1 A0 R/W" -- W=0
+
+#define OPCODEW (0b01000000) // Opcode for MCP23S17 with LSB (bit0) set to write (0), address OR'd in later, bits 1-3
+#define OPCODER (0b01000001) // Opcode for MCP23S17 with LSB (bit0) set to read (1), address OR'd in later, bits 1-3
+
+
+int MCP_setup(MCP* mcp, uint8_t address)
+{
+  mcp->_address = address;
+
+  if (MCP_byteWrite(mcp, IOCON, IOCON_INIT) == -1) {
+    error_log("MCP_byteWrite failure 0 inside MCP_setup");
+    printf("MCP_byteWrite failure 0 inside MCP_setup");
+
+    return  -1;
+  }
+
+  mcp->_modeCache = 0xFF;   // Default I/O mode is all input, 0xFFFF
+  mcp->_outputCache = 0x00; // Default output state is all off, 0x0000
+  mcp->_pullupCache = 0x00; // Default pull-up state is all off, 0x0000
+  mcp->_invertCache = 0x00; // Default input inversion state is not inverted, 0x0000
+
+  if (MCP_byteWrite(mcp, IODIR, mcp->_modeCache) == -1) {
+    error_log("MCP_byteWrite failure 1 inside MCP_setup");
+    printf("MCP_byteWrite failure 1 inside MCP_setup");
+
+    return -1;
+  } else if (MCP_byteWrite(mcp, IOCON, IOCON_INIT) == -1) {
+    error_log("MCP_byteWrite failure 2 inside MCP_setup");
+    printf("MCP_byteWrite failure 2 inside MCP_setup");
+
+    return -1;
+  }
+
+  return 0;
+};
+
+int MCP_byteWrite(MCP *mcp, uint8_t reg, uint8_t value)
+{ // Accept the register and byte
+
+  uint8_t tx_buf[3];
+  tx_buf[0] = OPCODEW | (mcp->_address << 1);
+  tx_buf[1] = reg;
+  tx_buf[2] = value;
+  uint8_t rx_buf[sizeof tx_buf];
+
+  struct spi_ioc_transfer spi;
+  memset(&spi, 0, sizeof(spi));
+  spi.tx_buf = (unsigned long)tx_buf;
+  spi.rx_buf = (unsigned long)rx_buf;
+  spi.len = sizeof tx_buf;
+  spi.delay_usecs = spi_delay;
+  spi.speed_hz = spi_speed;
+  spi.bits_per_word = spi_bpw;
+
+
+  // do the SPI transaction
+  if ((ioctl(spiFds, SPI_IOC_MESSAGE(1), &spi) < 0)) {
+    error_log("mcp23s08_write_reg: Error during SPI transaction.");
+    printf("mcp23s08_write_reg: Error during SPI transaction.");
+
+    return -1;
+  }
+
+  return 0; // return value for succesful execution
+
+}
+
+
+
+// MODE SETTING FUNCTIONS - BY PIN
+
+int MCP_pinMode(MCP *mcp, uint8_t pin, uint8_t mode)
+{ // Accept the pin # and I/O mode
+
+  if (pin > 16) {
+    // create and store error message
+    char error_msg[50];
+    sprintf(error_msg, "MCP_pinMode, pin out of bounds: %u", pin);
+    error_log(error_msg);
+
+    // display error message
+    printf(error_msg);
+
+    return -1;
+  }
+  if (mode == MCP_INPUT)
+  {                                // Determine the mode before changing the bit state in the mode cache
+    mcp->_modeCache |= 1 << (pin); // Since input = "HIGH", OR in a 1 in the appropriate place
+  }
+  else
+  {
+    mcp->_modeCache &= ~(1 << (pin)); // If not, the mode must be output, so and in a 0 in the appropriate place
+  }
+
+  if (MCP_byteWrite(mcp, IODIR, mcp->_modeCache) == -1) {
+    error_log("MCP_byteWrite failure inside MCP_pinMode");
+    printf("MCP_byteWrite failure inside MCP_pinMode");
+
+    return -1; // return -1 upon bytewrite failure
+  }
+
+  return 0;
+}
+
+// WRITE FUNCTIONS
+
+int MCP_pinWrite(MCP *mcp, uint8_t pin, uint8_t value)
+{
+
+  if (pin > 16)
+  {
+    // create and store error message
+    char error_msg[50];
+    sprintf(error_msg, "MCP_pinWrite, pin out of bounds: %u", pin);
+    error_log(error_msg);
+
+    // display error message
+    printf(error_msg);
+
+    return -1;
+  }
+ 
+
+  if (value) {
+    mcp->_outputCache |= 1 << (pin);
+  } else {
+    mcp->_outputCache &= ~(1 << (pin));
+  }
+  
+  if (MCP_byteWrite(mcp, GPIO, mcp->_outputCache) == -1) {
+    error_log("MCP_byteWrite failure inside MCP_pinWrite");
+    printf("MCP_byteWrite failure inside MCP_pinWrite");
+
+    return -1;
+  }
+
+  return 0;
+}
---
+++ lv-server/dac8164.c
@@ -0,0 +1,209 @@
+/*
+ * DAC8164.c
+ *
+ *  Created on: June 21, 2022
+ *      Author: vrusu
+ */
+
+#include "dac8164.h"
+#include "stdint.h"
+#include <stdio.h>
+#include "gpio.h"
+#include "MCP23S08.h"
+#include "utils.h"
+#include <unistd.h>
+
+#define DAC8164DELAY 20 // in uS
+
+int DAC8164_setup(DAC8164 *self, MCP *MCP, uint8_t sync, int sclk, uint8_t sdi, int enable_pin, uint8_t ldac_pin)
+{
+  self->_MCP = MCP;
+  self->_sync_pin = sync;
+  self->_sclk_pin = sclk;
+  self->_sdi_pin = sdi;
+  self->_enable_pin = enable_pin;
+  self->_ldac_pin = ldac_pin;
+
+  char error_msg[100];
+
+
+  if (MCP_pinMode(self->_MCP, self->_sync_pin, OUTPUT) == -1) {
+    sprintf(error_msg, "dac8164_setup _sync_pin MCP_pinMode fail: %u", self->_sync_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+  if (MCP_pinWrite(self->_MCP, self->_sync_pin, HIGH) == -1) {
+    sprintf(error_msg, "dac8164_setup _sync_pin MCP_pinWrite fail: %u", self->_sync_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+
+
+  if (MCP_pinMode(self->_MCP, self->_sclk_pin, OUTPUT) == -1) {
+    sprintf(error_msg, "dac8164_setup _sclk_pin MCP_pinMode fail: %u", self->_sclk_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+  if (MCP_pinWrite(self->_MCP, self->_sclk_pin, LOW) == -1) {
+    sprintf(error_msg, "dac8164_setup _sync_pin MCP_pinWrite fail: %u", self->_sclk_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+
+  if (MCP_pinMode(self->_MCP, self->_sdi_pin, OUTPUT) == -1) {
+    sprintf(error_msg, "dac8164_setup _sync_pin MCP_pinMode fail: %u", self->_sdi_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+  if (MCP_pinWrite(self->_MCP, self->_sdi_pin, LOW) == -1) {
+    sprintf(error_msg, "dac8164_setup _sync_pin MCP_pinWrite fail: %u", self->_sdi_pin);
+    error_log(error_msg);
+
+    return -1;
+  }
+
+  
+
+
+  return 0;
+}
+
+
+void DAC8164_write(DAC8164 *self, uint32_t data)
+{
+  char error_msg[50];
+
+
+  if (MCP_pinWrite(self->_MCP, self->_sclk_pin, 0) == -1) {
+    sprintf(error_msg, "dac8164_write _sclk_pin fail: %u", self->_sclk_pin);
+    error_log(error_msg);
+  }
+  
+  if (MCP_pinWrite(self->_MCP, self->_sync_pin, 0) == -1) {
+    sprintf(error_msg, "dac8164_write _sync_pin fail: %u", self->_sync_pin);
+    error_log(error_msg);
+  }
+  usleep(DAC8164DELAY);
+
+  for (int i=23;i>=0;i--){
+    uint8_t thisbit;
+    if ((0x1<<i) & data)
+      thisbit = 1;
+    else
+      thisbit = 0;
+
+
+    if (MCP_pinWrite(self->_MCP, self->_sdi_pin, thisbit) == -1) {
+      sprintf(error_msg, "dac8164_write _sdi_pin fail: %u", self->_sdi_pin);
+      error_log(error_msg);
+    }
+    usleep(DAC8164DELAY);
+
+    if (MCP_pinWrite(self->_MCP, self->_sclk_pin, 1) == -1) {
+      sprintf(error_msg, "dac8164_write _sclk_pin on fail: %u", self->_sclk_pin);
+      error_log(error_msg);
+    }
+    usleep(DAC8164DELAY);
+
+    if (MCP_pinWrite(self->_MCP, self->_sclk_pin, 0) == -1) {
+      sprintf(error_msg, "dac8164_write _sclk_pin off fail: %u", self->_sclk_pin);
+      error_log(error_msg);
+    }
+    usleep(DAC8164DELAY);
+    
+  }
+
+  if (MCP_pinWrite(self->_MCP, self->_sync_pin, 1) == -1) {
+    sprintf(error_msg, "dac8164_write _sync_pin on fail: %u", self->_sync_pin);
+    error_log(error_msg);
+  }
+  
+}
+
+
+void DAC8164_setReference(DAC8164 *self, uint16_t reference)
+{
+  uint32_t data = DAC_MASK_PD0;
+
+  // set reference mde
+  data |= reference;
+
+  DAC8164_write(self, data);
+
+}
+
+
+void DAC8164_writeChannel(DAC8164 *self, uint8_t channel, uint16_t value)
+{
+  uint32_t data ;
+  uint32_t dac_mask;
+
+  uint8_t mod_channel = channel % 4;
+
+  if ((channel - mod_channel) == 8) {
+    dac_mask = DAC_MASK_2;
+  }
+  else if ((channel - mod_channel) == 4) {
+    dac_mask = DAC_MASK_1;
+  }
+  else {
+    dac_mask = DAC_MASK_0;
+  }
+  mod_channel = mod_channel + 1;
+
+
+  if (mod_channel == DAC_CHANNEL_A)
+    data = DAC_SINGLE_CHANNEL_UPDATE  | dac_mask ;
+
+  else if (mod_channel == DAC_CHANNEL_B)
+    data = DAC_SINGLE_CHANNEL_UPDATE | DAC_MASK_DACSEL0 | dac_mask ;
+
+  else if (mod_channel == DAC_CHANNEL_C)
+    data = DAC_SINGLE_CHANNEL_UPDATE| DAC_MASK_DACSEL1 | dac_mask ;
+
+  else if (mod_channel == DAC_CHANNEL_D)
+    data = DAC_SINGLE_CHANNEL_UPDATE | DAC_MASK_DACSEL1 | DAC_MASK_DACSEL0 | dac_mask ;
+
+  else if (mod_channel == DAC_CHANNEL_ALL)
+    data = DAC_BROADCAST_UPDATE | DAC_MASK_DACSEL1 ;
+  else
+    // avoid writing bad data
+    return;
+
+  // value is 12 MSB bits (last LSB nibble to 0)
+
+  data |= value << 2;
+  // Send to chip
+  DAC8164_write (self, data);
+}
+
+/*
+void DAC8164_setChannelPower(DAC8164 *self, uint8_t channel, uint16_t power)
+{
+  // Default we'll set power
+  uint32_t data = power | DAC_MASK_PD0 ;
+
+  if (channel == DAC_CHANNEL_A)
+    data |= DAC_SINGLE_CHANNEL_UPDATE;
+
+  else if (channel == DAC_CHANNEL_B)
+    data |= DAC_SINGLE_CHANNEL_UPDATE | DAC_MASK_DACSEL0 ;
+
+  else if (channel == DAC_CHANNEL_C)
+    data |= DAC_SINGLE_CHANNEL_UPDATE| DAC_MASK_DACSEL1 ;
+
+  else if (channel == DAC_CHANNEL_D)
+    data |= DAC_SINGLE_CHANNEL_UPDATE | DAC_MASK_DACSEL1 | DAC_MASK_DACSEL0 ;
+
+  else if (channel == DAC_CHANNEL_ALL)
+    data |= DAC_BROADCAST_UPDATE | DAC_MASK_DACSEL1 ;
+
+  // Send to chip
+  DAC8164_write (self, data);
+}
+*/
